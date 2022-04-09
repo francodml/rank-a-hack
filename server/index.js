b2203e0dab7b593a0787d049c4a60cf42850aa89
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-09 10:17:34 -0300

    Set up basic database connection

--- server/index.js
+++ server/index.js
@@ -5,8 +5,23 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-const PORT = process.env.PORT || 3001
+app.use( bodyParser.json({limit: "30mb", extended: true}) );
+app.use( bodyParser.urlencoded({limit: "30mb", extended: true}) );
+app.use( cors() );
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+const CONNECTION_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.vql6v.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect( CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch( err => console.log(err.message) );
+
+mongoose.set('useFindAndModify', false);
