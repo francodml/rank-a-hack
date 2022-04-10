b5bcef11004ec3ff0f7ab0c516dd524ccaa43ccd
francodml <7284418+francodml@users.noreply.github.com>
2022-04-10 11:13:53 -0300

    Try and add some more routes

--- server/apiroutes/hackathons.js
+++ server/apiroutes/hackathons.js
@@ -3,7 +3,15 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('All hackathons');
+});
+
+router.get('/getOne', (req, res) => {
+    res.send('Specific hackathon');
+});
+
+router.post('/', (req, res) => {
+    res.send('Create hackathon');
 });
 
 export default router;
