e26c8fe6bb0c403a3900ff71c5317895ba80a528
francodml <7284418+francodml@users.noreply.github.com>
2022-04-11 14:18:12 -0300

    Export DevelopmentSchema

--- server/models/development.js
+++ server/models/development.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const DevelopmentSchema = new Schema({
+export const DevelopmentSchema = new Schema({
     name: String,
     description: String,
     developerId: Schema.Types.ObjectId,
-    hackathonId: Schema.Types.ObjectId,
     ranking: { type: Number, default: 0 }
 });
 
