04d1339dece9b8c50258e4f2f1a288f2be3c091c
francodml <7284418+francodml@users.noreply.github.com>
2022-04-11 12:51:51 -0300

    Remove HackathonParticipantSchema Seems like duplicate data? All data pertinent to participation is stored in developments

--- server/models/hackathon.js
+++ server/models/hackathon.js
@@ -1,23 +1,16 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-
-const HackathonParticipantSchema = new Schema({
-    userId: Schema.Types.ObjectId,
-    developmentId: Schema.Types.ObjectId,
-    ranking: { type: Number, default: 0 },
-});
+import {DevelopmentSchema} from './development.js';
 
 const HackathonSchema = new Schema({
     name: String,
     description: String,
     location: String,
     id: {type: Number, default: 0, unique: true, dropups: true},
-    participants: [HackathonParticipantSchema],
+    entries: [DevelopmentSchema],
     startDate: { type: Date, default: Date.now },
 });
 
 const Hackathon = mongoose.model('Hackathon', HackathonSchema);
-
-export const HackathonParticipant = mongoose.model('HackathonParticipant', HackathonParticipantSchema);
 
 export default Hackathon;
