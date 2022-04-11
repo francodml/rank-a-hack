cd713545aab4f084441e5a205165048a6c57009a
francodml <7284418+francodml@users.noreply.github.com>
2022-04-11 12:51:17 -0300

    Add incremental userId, avatarUrl, email to user

--- server/models/user.js
+++ server/models/user.js
@@ -3,10 +3,13 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     username: {type: String, unique:true, dropups:true},
+    userId: {type: Number, default: 0, unique: true, dropups: true},
     firstName: String,
     lastName: String,
     location: String,
     hackathons: [Schema.Types.ObjectId],
+    avatarUrl: String,
+    email: String,
 });
 
 const User = mongoose.model('User', UserSchema);
