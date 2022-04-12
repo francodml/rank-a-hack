1f5637f9cc63dd9c0b4a12dcd95ff5872c486fb9
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-12 14:06:21 -0300

    Populate /user paths

--- server/apiroutes/users.js
+++ server/apiroutes/users.js
@@ -1,16 +1,38 @@
 import express from 'express';
+import User from '../models/user.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('All users?');
+    User.find()
+        .then(users => res.send(users))
+        .catch(err => res.status(500).send(err));
 });
 
 router.get('/:id', (req, res) => {
-    res.send(`Specific user @${req.params.id}`);
+    User.find({userId: req.params.id})
+        .then(user => res.send(user))
+        .catch(err => res.status(500).send(err));
 });
 
-router.post('/', (req, res) => {
-    res.send('Create user');
+router.post('/', async (req, res) => {
+    var users = req.body;
+    const count = await User.estimatedDocumentCount();
+    const ids = [];
+    for (let i = 0; i < users.length; i++) {
+        users[i].userId = count + i;
+        ids.push(users[i].userId);
+    }
+    User.insertMany(users)
+        .then(() => {
+            res.status(201);
+            res.json(ids);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send(err);
+        });
+
 });
 
+export default router;
