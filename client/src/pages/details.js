efa8ef8e6ca2283d4a5024b85da5b9bcd59a2cf4
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-14 23:32:17 -0300

    Details page skeleton

---
+++ client/src/pages/details.js
@@ -0,0 +1,12 @@
+//hackathon details page
+
+export default function Details() {
+    return (
+        <div className="Details">
+        <h1>Details</h1>
+        <p>
+            This is the details page.
+        </p>
+        </div>
+    );
+}
