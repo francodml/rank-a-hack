162effc164214c17f8e723007c528364e781d93a
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-18 14:44:35 -0300

    Add top 3 entries to listing

--- client/src/components/hackathon.js
+++ client/src/components/hackathon.js
@@ -8,11 +8,28 @@ export default function Hackathon({ hackathon }) {
 
     let nav = useNavigate();
 
+    var entries = [...hackathon.entries];
+    entries.sort((a, b) => { return b.ranking - a.ranking });
+    console.log(entries);
+
+    const topEntries = entries.slice(0, 3);
+
     return (
         <div className="Hackathon" onClick={ event => { event.preventDefault(); nav(`/details/${hackathon.id}`)}}>
-            <p className='Hackathon__title'>{hackathon.name}</p>
-            <p className='Hackathon__date'>{formattedDate}</p>
-            <p className='Hackathon__description'>{hackathon.description}</p>
+            <p className='title'>{hackathon.name}</p>
+            <p className='subtitle'>{formattedDate}</p>
+            <p className='description'>{hackathon.description}</p>
+            <h1 className='subtitle'>Top entries</h1>
+            <div className='entries'>
+                {topEntries.map(entry => {
+                    return (
+                        <div className='entry'>
+                            <p className='title'>{entry.name}</p>
+                            <p className='subtitle'>{entry.ranking.toFixed(1)}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
