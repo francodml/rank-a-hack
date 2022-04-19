686a796dd41cd09cdfa87a9fd60bc570ee64ac2e
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-19 03:29:51 -0300

    Remove debug print

--- client/src/components/userdetail.js
+++ client/src/components/userdetail.js
@@ -1,7 +1,6 @@
 import './styles/developers.scss'
 
 export default function UserDetail({ user }) {
-  console.log(user);
     return (
         <div className="userCard">
             <img className='profilePic' src={user.avatarUrl} alt={user.name} />
