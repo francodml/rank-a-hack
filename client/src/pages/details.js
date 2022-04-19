54016504f724e904492096e38d36de42eb68555e
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-19 03:29:17 -0300

    Populate details page

--- client/src/pages/details.js
+++ client/src/pages/details.js
@@ -1,14 +1,36 @@
+import './styles/details.scss';
 import { useParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useGetHackathonQuery } from "../redux/mainAPI";
+import Developers from "../components/developers";
 
 export default function Details() {
     let urlParams = useParams();
+    const { data, loading, error } = useGetHackathonQuery(urlParams.id);
+
+    if (error){
+        return (
+            <Navigate to="/" />
+        )
+    }
+
+    if (loading) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
     return (
-        <div className="Details">
-        <h1>Details</h1>
-        <p>
-            This is the details page.
-            {urlParams.id}
-        </p>
-        </div>
+        data ? 
+            <div className="Details">
+                <p className="title">{data.name}</p>
+                <p className="description">
+                    {data.description}
+                </p>
+                <div className="entries">
+                    <Developers hackathonId={data.id} />
+                </div>
+            </div>
+         : null
     );
 }
