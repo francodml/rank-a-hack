9c6e7f41555223e9a5cab6dc3039a0e65b202f43
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-19 03:28:09 -0300

    Developers list component

---
+++ client/src/components/developers.js
@@ -0,0 +1,30 @@
+//developers list component
+import UserDetail from "./userdetail";
+import './styles/developers.scss'
+import { useGetDevelopersFromHackathonQuery } from "../redux/mainAPI";
+
+export default function Developers({ hackathonId }) {
+  const { data, loading, error } = useGetDevelopersFromHackathonQuery(hackathonId);
+
+  if (error) {
+    return <p>Error: {error.status}</p>;
+  }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const elm = <>
+    {data? data.map(user => {
+        return (
+            <UserDetail key={user.userId} user={user} />
+        );
+    }) : null}
+  </>
+
+  return (
+      elm
+  );
+
+
+}
