ea0b3d8539309fa0828fde352655052a899728d1
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-19 22:26:28 -0300

    Style modules for UserDetail

--- client/src/components/userdetail.js
+++ client/src/components/userdetail.js
@@ -1,11 +1,13 @@
-import './styles/userdetail.scss'
+import styles from './styles/userdetail.module.scss';
 
 export default function UserDetail({ user, compact }) {
 
+    const cardstyles = [styles.userCard, compact ? styles.small : ''].join(' ');
+
     return (
-        <div className={`userCard ${compact ? 'small' : ''}`}>
-            <img className='profilePic' src={user.avatarUrl} alt={user.name} />
-            <p className='name'>{user.username}</p>
+        <div className={cardstyles}>
+            <img className={styles.profilePic} src={user.avatarUrl} alt={user.name} />
+            <p className={styles.name}>{user.username}</p>
         </div>
     );
 }
