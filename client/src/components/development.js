e8613a661a5a3faf58ece1f68927b8602556b95f
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-20 16:34:35 -0300

    Compact variant for Development entry component

--- client/src/components/development.js
+++ client/src/components/development.js
@@ -1,12 +1,12 @@
 import styles from './styles/development.module.scss';
 import UserDetail from "./userdetail";
 
-export default function Development ({ entry }) {
+export default function Development ({ entry, compact }) {
     return (
         <div className={styles.Development}>
             <p className={styles.Title}>{entry.name}</p>
             <p className={styles.Subtitle}>ranked {entry.ranking.toFixed(1)}/10.0</p>
-            <p className={styles.Description}>{entry.description}</p>
+            {!compact ? <p className={styles.Description}>{entry.description}</p> : null}
             <div className={styles.UserDetail}>
                 <UserDetail user={entry.developerId} compact />
             </div>
