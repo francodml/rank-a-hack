b1bf2e209de879e266136015b94a96a9edfcdeeb
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-19 22:27:03 -0300

    Style modules for development component

--- client/src/components/development.js
+++ client/src/components/development.js
@@ -1,12 +1,15 @@
+import styles from './styles/development.module.scss';
 import UserDetail from "./userdetail";
 
 export default function Development ({ entry }) {
     return (
-        <div className='entry'>
-            {entry.name}
-            <p className='title'>{entry.name}</p>
-            <p className='subtitle'>ranked {entry.ranking.toFixed(1)}/10.0</p>
-            <UserDetail user={entry.developerId} compact />
+        <div className={styles.Development}>
+            <p className={styles.Title}>{entry.name}</p>
+            <p className={styles.Subtitle}>ranked {entry.ranking.toFixed(1)}/10.0</p>
+            <p className={styles.Description}>{entry.description}</p>
+            <div className={styles.UserDetail}>
+                <UserDetail user={entry.developerId} compact />
+            </div>
         </div>
     )
 }
