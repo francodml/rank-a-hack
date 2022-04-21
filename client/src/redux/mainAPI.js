d25e43c9ce9339cf388393857d62cd4f2bdeda7a
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-21 14:48:38 -0300

    Use axios for queries More flexibility

--- client/src/redux/mainAPI.js
+++ client/src/redux/mainAPI.js
@@ -1,36 +1,59 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import axios from 'axios';
+
+const axiosBaseQuery = ({ baseUrl } = { baseUrl: '' }) => 
+    async ({url, method, data, params}) => {
+        try {
+            const result = await axios({url: baseUrl + url, method, data, params});
+            return {data: result.data}
+        }
+        catch (error) {
+            let err = error;
+            return {
+                error: {
+                    status: err.response?.status,
+                    data: err.response?.data || err.message,
+                }
+            }
+        }
+    }
 
 export const mainApi = createApi({
     reducerPath: 'mainApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:3001/' }),
+    // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+    baseQuery: axiosBaseQuery({ baseUrl: '/api/' }),
 
     tagTypes: ['Hackathon', 'User'],
     endpoints: (builder) => ({
         getHackathons: builder.query({
-            query: () => 'hackathons',
+            query: () => ({url: 'hackathons', method: 'get'}),
             providesTags: ['Hackathon'],
         }),
 
         getHackathon: builder.query({
-            query: (id) => `hackathons/${id}`,
+            query: (id) => ({url: `hackathons/${id}`, method: 'get'}),
             providesTags: ['Hackathon'],
         }),
 
         getDevelopersFromHackathon: builder.query({
-            query: (id) => `hackathons/${id}/devs`,
+            query: (id) => ({url: `hackathons/${id}/devs`, method: 'get'}),
             providesTags: ['User'],
         }),
 
         getUser: builder.query({
-            query: (id) => `users/${id}`,
+            query: (id) => ({url: `users/${id}`, method: 'get'}),
             providesTags: ['User'],
         }),
 
         getUsers: builder.query({
-            query: () => `users`,
+            query: () => ({url: `users`, method: 'get'}),
             providesTags: ['User'],
         }),
+
+        sendError: builder.query({
+            query: (error) => ({url: `errors`, method: 'post', data: error}),
+        })
     }),
 })
 
-export const { useGetHackathonsQuery, useGetHackathonQuery, useGetUserQuery, useGetUsersQuery, useGetDevelopersFromHackathonQuery } = mainApi;
+export const { useGetHackathonsQuery, useGetHackathonQuery, useGetUserQuery, useGetUsersQuery, useGetDevelopersFromHackathonQuery, useSendErrorQuery } = mainApi;
