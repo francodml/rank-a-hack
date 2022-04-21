c2e7e2e8aab2d8b7f0542f2b1486f46c4cc1823f
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-21 00:20:25 -0300

    scss module for navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,14 +1,16 @@
+import styles from './styles/navbar.module.scss';
 import logo from '../logo.svg';
 import { Link, NavLink } from 'react-router-dom';
-import './styles/navbar.css';
 
 export default function Navbar(){
     return (
-        <div className='Header'>
-            <img src={logo} className="Header-Logo" alt="logo" />
-            <Link to="/" className="Header-Title">Rank-a-Hack</Link>
-            <div className='VerticalSplit'></div>
-            <NavLink to="/" className={'Header-NavButton'}>Hackathons</NavLink>
+        <div className={styles.Navbar}>
+            <img src={logo} className={styles.Logo} alt="logo" />
+            <Link to="/" className={styles.Title}>Rank-a-Hack</Link>
+            <div className={styles.VerticalSplit}></div>
+            <NavLink to="/" className={styles.NavButton}>Hackathons</NavLink>
+            <NavLink to="devranking" className={styles.NavButton}>Top Devs</NavLink>
+
         </div>
     );
 }
