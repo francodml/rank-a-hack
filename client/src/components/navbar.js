a9de180c5856c4d5cc1269dd9be48affe34defe2
Win7yes <7284418+francodml@users.noreply.github.com>
2022-04-21 06:11:05 -0300

    Use navbutton styles in navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,13 +3,18 @@ import logo from '../logo.svg';
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Navbar(){
+
+    const NavButtonStyle = ({isActive}) =>{
+        return `${styles.NavButton} ${isActive ? styles.active : ''}`;
+    }
+
     return (
         <div className={styles.Navbar}>
             <img src={logo} className={styles.Logo} alt="logo" />
             <Link to="/" className={styles.Title}>Rank-a-Hack</Link>
             <div className={styles.VerticalSplit}></div>
-            <NavLink to="/" className={styles.NavButton}>Hackathons</NavLink>
-            <NavLink to="devranking" className={styles.NavButton}>Top Devs</NavLink>
+            <NavLink to="/" className={NavButtonStyle}>Hackathons</NavLink>
+            <NavLink to="devranking" className={NavButtonStyle}>Top Devs</NavLink>
 
         </div>
     );
